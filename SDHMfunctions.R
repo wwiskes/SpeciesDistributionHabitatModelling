01ce97ebc0c724725e7592c5c034364007f95dc2
dwrwilliam <wwiskes@utah.gov>
2022-06-29 08:58:00 -0600

    Update SDHMfunctions.R

--- SDHMfunctions.R
+++ SDHMfunctions.R
@@ -207,7 +207,7 @@ glmFunction <- function(cutData, rasters) {
   #table
   plot.new()
   table <- summary(mod2.LR)
-  t <- as.data.frame(table$coefficients) %>% mutate_if(is.numeric, ~round(., 5))
+  t <- as.data.frame(table$coefficients) %>% mutate_if(is.numeric, ~round(., 5)) %>% mutate_if(is.numeric, as.character(.))
   grid.draw(tableGrob(t))
   #start accuracy assessment
 
